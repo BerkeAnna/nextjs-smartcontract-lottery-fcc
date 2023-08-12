06c623566d79edd3e8adbd9b8c455414492bfeaf
BerkeAnna <berke.anna0803@gmail.com>
2023-08-12 17:24:11 +0200

    nextjs setup

--- pages/api/index.js
+++ pages/api/index.js
@@ -1,16 +1,14 @@
 import Image from 'next/image'
 import styles from "../styles/globals.css"
-import Head from 'next/head'
 
 export default function Home() {
   return (
    <div className={styles.container}>
       <Head>
-        <title>Smart contract lottery</title>
-        <meta name = "description" content="Our smart contract lottery" ></meta>
+        <title>Cretae app</title>
+        <meta name = "description" ></meta>
         <link rel= "icon" href="/favicon.ico"></link>
       </Head>
-      Hello!
    </div>
   )
 }
